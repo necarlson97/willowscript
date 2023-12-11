3d31ecf2c088436e9f702125d2418bd44b8bce1b
necarlson97 <nilscarlson1997@gmail.com>
2023-12-11 09:45:05 -0500

    Basic functionally

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # willowscript
 A small website for writing out a beautiful vertical cipher script, willowscript, by ['TheAlchemyBook'](www.tiktok.com/@thealchemybook)
+
+[screenshot](lazy-dog.png)
+[wire](wire.jpg)
+
+# Work In Progress
+Still needs to be done:
+* Numbers!
+* Options for changing question-mark styles, font size, etc
--- script.js
+++ script.js
@@ -3,14 +3,46 @@ function updateImages() {
     var imageContainer = document.getElementById('imageContainer');
     imageContainer.innerHTML = ''; // Clear previous images
 
+    var wordDiv = null;
+
     for (var i = 0; i < text.length; i++) {
-        if (text[i] === ' ') {
+        if (text[i].match(/\s/)) { // Check for whitespace
+            wordDiv = null; // End the current word
             var br = document.createElement('br');
             imageContainer.appendChild(br);
         } else {
+            if (!wordDiv) {
+                wordDiv = document.createElement('div'); // Start a new word
+                wordDiv.className = 'word';
+                imageContainer.appendChild(wordDiv);
+            }
             var img = document.createElement('img');
-            img.src = 'images/' + text[i].toLowerCase() + '.png';
-            imageContainer.appendChild(img);
+            img.alt = ''; // Don't show image if it is missing
+            img.src = getImageSource(text, i);
+            wordDiv.appendChild(img);
         }
     }
 }
+
+function getImageSource(text, i) {
+    if (text[i] === '"') {
+        if (atStartOfWord(text, i)) return 'images/top-quote.png';
+        return 'images/bottom-quote.png';
+    } else if (text[i] === '?') {
+        if (atStartOfWord(text, i)) return 'images/top-question.png';
+        return 'images/bottom-question.png';
+    } else if (text[i] === '.') {
+        return 'images/period.png'
+    } else if (text[i] === ',') {
+        return 'images/comma.png'
+    } else {
+        return 'images/' + text[i].toLowerCase() + '.png';
+    }
+}
+
+function atStartOfWord(text, i) {
+    // Returns true if the given index is at the start of a word
+    return (i < text.length - 1 && text[i + 1] !== ' ');
+}
+
+window.onload = updateImages; // Call updateImages on window load
